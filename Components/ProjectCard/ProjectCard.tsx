735b5cc281db9bbbd03387383553496e777318ea
YogeshDhengale <yd30668497@gmail.com>
2025-12-29 17:52:26 +0530

    Fix: carousel issue resolved

--- Components/ProjectCard/ProjectCard.tsx
+++ Components/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import ProductCarousel from "../ProductCarousel/ProductCarousel";
 import Link from "next/link";
@@ -22,7 +24,10 @@ function ProjectCard({ project }: { project: IProjectProp }) {
       <div className="mb-4 group-hover:underline">
         <h3 className="text-base font-semibold">{project.title}</h3>
       </div>
-      <div onClick={(e) => e.stopPropagation()} className="rounded-xl max-w-full aspect-square bg-linear-180 from-[#7a19c4] via-[#460e70] to-[#430e6b]">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="rounded-xl max-w-full aspect-square bg-linear-180 from-[#7a19c4] via-[#460e70] to-[#430e6b]"
+      >
         <ProductCarousel images={project.images} />
       </div>
     </Link>
